9694649409b8885073ee82d21aa5a762be5bb801
Noorja <siddhaigarunoorjahan@gmail.com>
2026-02-25 12:03:46 +0530

    Codedeploy

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -4,12 +4,24 @@ resource "aws_lb" "noor_alb" {
   subnets            = [aws_subnet.noor_subnet.id]
   security_groups    = [aws_security_group.noor_alb_sg.id]
 }
+
+# ✅ Blue Target Group
+resource "aws_lb_target_group" "noor_blue_tg" {
+  name     = "noor-blue-tg"
+  port     = 1337
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.noor_vpc.id
+}
+
+# ✅ Green Target Group
 resource "aws_lb_target_group" "noor_green_tg" {
   name     = "noor-green-tg"
   port     = 1337
   protocol = "HTTP"
   vpc_id   = aws_vpc.noor_vpc.id
 }
+
+# ✅ Listener (Initially pointing to Blue)
 resource "aws_lb_listener" "noor_listener" {
   load_balancer_arn = aws_lb.noor_alb.arn
   port              = 80
