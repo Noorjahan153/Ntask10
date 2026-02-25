bf23f2ea07da214cb978feaac969e2991e099ffe
Noorja <siddhaigarunoorjahan@gmail.com>
2026-02-25 13:18:56 +0530

    Codedeploy1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 ############################################
-# Default VPC + Subnets
+# Provider VPC
 ############################################
 
 data "aws_vpc" "default" {
@@ -14,25 +14,84 @@ data "aws_subnets" "default" {
 }
 
 ############################################
-# ECS Cluster
-############################################
-
-resource "aws_ecs_cluster" "strapi_cluster" {
-  name = "strapi-cluster"
-}
-
-############################################
-# ALB
+# Security Groups (Existing)
 ############################################
 
 data "aws_security_group" "alb_sg" {
   name = "noor-alb-sg"
 }
 
-resource "aws_lb" "strapi_alb" {
-  name               = "strapi-alb"
+############################################
+# ALB
+############################################
+
+resource "aws_lb" "noor_alb" {
+  name               = "noor-alb"
   load_balancer_type = "application"
 
   security_groups = [data.aws_security_group.alb_sg.id]
   subnets         = data.aws_subnets.default.ids
+}
+
+############################################
+# Target Groups
+############################################
+
+resource "aws_lb_target_group" "noor_blue_tg" {
+  name     = "noor-blue-tg"
+  port     = 1337
+  protocol = "HTTP"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_lb_target_group" "noor_green_tg" {
+  name     = "noor-green-tg"
+  port     = 1337
+  protocol = "HTTP"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+############################################
+# Listener
+############################################
+
+resource "aws_lb_listener" "noor_listener" {
+  load_balancer_arn = aws_lb.noor_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+
+    target_group_arn = aws_lb_target_group.noor_blue_tg.arn
+  }
+}
+
+############################################
+# ECS Cluster
+############################################
+
+resource "aws_ecs_cluster" "noor_cluster" {
+  name = "noor-cluster"
+}
+
+############################################
+# ECS Service
+############################################
+
+resource "aws_ecs_service" "noor_service" {
+  name            = "noor-service"
+  cluster         = aws_ecs_cluster.noor_cluster.id
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  task_definition = "PLACE_TASK_DEFINITION_ARN"
+
+  network_configuration {
+    subnets         = data.aws_subnets.default.ids
+    assign_public_ip = true
+    security_groups = [data.aws_security_group.alb_sg.id]
+  }
 }
