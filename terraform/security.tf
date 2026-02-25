42acaab0bee3ecba04185fca0c39a87493d3d647
Noorja <siddhaigarunoorjahan@gmail.com>
2026-02-25 13:09:52 +0530

    Codedeploy1

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,46 +1,14 @@
-# ALB Security Group
-resource "aws_security_group" "noor_alb_sg" {
-  name   = "noor-alb-sg"
-  vpc_id = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Default VPC
+data "aws_vpc" "default" {
+  default = true
 }
 
-# ECS Security Group
-resource "aws_security_group" "noor_ecs_sg" {
-  name   = "noor-ecs-sg"
-  vpc_id = data.aws_vpc.default.id
+# Existing ALB Security Group
+data "aws_security_group" "noor_alb_sg" {
+  name = "noor-alb-sg"
+}
 
-  ingress {
-    from_port       = 1337
-    to_port         = 1337
-    protocol        = "tcp"
-    security_groups = [aws_security_group.noor_alb_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Existing ECS Security Group
+data "aws_security_group" "noor_ecs_sg" {
+  name = "noor-ecs-sg"
 }
