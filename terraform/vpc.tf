2e3833bbd6f4bb1d6bd60ba13aac7c812e97eb6a
Noorja <siddhaigarunoorjahan@gmail.com>
2026-02-25 12:43:02 +0530

    Codedeploy

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,19 +1,20 @@
-resource "aws_vpc" "noor_vpc" {
-  cidr_block = "10.0.0.0/16"
+# Use Default VPC (Recommended for DevOps Projects)
+data "aws_vpc" "default" {
+  default = true
 }
 
-# Subnet 1 (AZ-a)
+# Subnet 1
 resource "aws_subnet" "noor_subnet_a" {
-  vpc_id                  = aws_vpc.noor_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.48.0/20"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
-# Subnet 2 (AZ-b)
+# Subnet 2
 resource "aws_subnet" "noor_subnet_b" {
-  vpc_id                  = aws_vpc.noor_vpc.id
-  cidr_block              = "10.0.2.0/24"
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.64.0/20"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
